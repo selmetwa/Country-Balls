6bae9f53537308b862f7714b5fd1cba0639168d9
Sherif Elmetwally <sge5@njit.edu>
2023-08-25 19:31:17 -0400

    add link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,7 @@
   <p>
     Data from <a href="https://data.worldbank.org/indicator" target="_blank">World Bank Indicators</a>
   </p>
+  <p>Source code <a href="https://github.com/selmetwa/sveltekit_vis" target="_blank">here</a></p>
 </footer>
 
 <style>
